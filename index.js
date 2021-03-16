7e639324971ef9b2470b8221725718197c78ad05
sangrae-cho <tkdfo93@gamil.com>
2021-03-17 08:01:16 +0900

    Feat#4: basic server setting completed. add session option, morgan logger and make it possible to route

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express');
+// const router = require('./Routers');
+const session = require('express-session');
 const cors = require('cors');
-const app = express();
-const port = 5000;
+const morgan = require('morgan');
+// const controller = require('./controllers');
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(
+  morgan(':method :url :stauts :res[content-length] - :response-times ms')
+);
+
+app.use(
+  session({
+    secret: '@A!B$',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      domain: '*',
+      path: '/*',
+      maxAge: 24 * 3600 * 1000,
+      sameSite: 'none',
+      httpOnly: true,
+      secure: true
+    }
+  })
+)
 app.use(cors());
 app.use(express.json());
 
+
+app.use('/', router);
+app.use('/user',router);
+
 app.get('/', (req, res) => {
-  res.status(200).send('연결성공');
+  res.status(200).send({ message: 'Connect Successfully'});
 });
 
 app.listen(port, () => {
-  console.log('server on port ' + port);
+  console.log('Server on port ' + port);
 });
