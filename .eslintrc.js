4c79a2b460f834229235a1c3221769888239f09b
duck-moon9392 <66950387+duck-moon9392@users.noreply.github.com>
2021-03-17 15:42:22 +0900

    Feature#3: server basic setting with eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+};
