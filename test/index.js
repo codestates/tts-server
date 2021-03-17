d9c5840a159e3e1038b3d1ea5e9f6aba0e5f9af8
duck-moon9392 <66950387+duck-moon9392@users.noreply.github.com>
2021-03-17 15:06:10 +0900

    빈 커밋

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
+const test = "사용하지 않는 변수";
 app.use(cors());
 app.use(express.json());
 
@@ -13,5 +14,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("server on port " + port);
 });
-
-const test = "사용하지 않는 변수";
