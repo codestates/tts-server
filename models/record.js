3887bc3c534440b93a4f4c744fd64ca96a0628b0
duck-moon9392 <66950387+duck-moon9392@users.noreply.github.com>
2021-03-18 14:31:43 +0900

    Feature11: DB create

---
+++ models/record.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class record extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  record.init({
+    day: DataTypes.INTEGER,
+    time: DataTypes.INTEGER,
+    recordName: DataTypes.STRING,
+    week: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'record',
+  });
+  return record;
+};
